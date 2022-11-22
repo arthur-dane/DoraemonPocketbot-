a9e15ebac439bd26c6a761ed763a6a2764bbe27d
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2022-11-22 17:32:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001550734540'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movies_halt_request_group)
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movies_halt_request_group')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
